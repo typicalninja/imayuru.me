7af144e762f38fd86430bbf26b8040d185a900f8
typicalninja <65993466+typicalninja@users.noreply.github.com>
2026-03-06 01:06:33 +0530

    feat: add personal website content and styling
    Includes initial content for the personal website, social links, and basic styling.
    Adds dependencies for fonts and icons.
    Creates a constants file for social links.
    Updates the index page with personal information and links.
    Adds global CSS for layout and typography.

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const Links = {
+    github: "https://github.com/typicalninja",
+    instagram: "https://www.instagram.com/imayuru_0/",
+    linkedin: "https://www.linkedin.com/in/imayuru/",
+}
