ffa760461c4ca01eb64d0cc30762ea62d66acb6e
typicalninja <65993466+typicalninja@users.noreply.github.com>
2026-03-06 09:31:16 +0530

    fix: remove instagram link from constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,4 @@
 export const Links = {
     github: "https://github.com/typicalninja",
-    instagram: "https://www.instagram.com/imayuru_0/",
     linkedin: "https://www.linkedin.com/in/imayuru/",
 }
