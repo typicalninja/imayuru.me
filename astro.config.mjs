4c12bca6477719373848a3cf9a584e5369a8b81d
typicalninja <65993466+typicalninja@users.noreply.github.com>
2026-03-22 12:49:40 +0530

    feat: add sitemap integration
    Adds the @astrojs/sitemap integration to generate an XML sitemap for the site.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://imayuru.me",
+  site: "https://imayuru.me",
+  integrations: [sitemap()],
 });
